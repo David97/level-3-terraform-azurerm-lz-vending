aa7821a6ea7e9778ee8b6a2aef0fbc0a9b175551
Henry Cheung <henry.cheung@softwareone.com>
2024-10-31 15:52:51 +0800

    Test provider

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
 terraform {
+  required_version = ">= 1.4.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.7.0"
     }
     azapi = {
-      source = "Azure/azapi"
+      source  = "azure/azapi"
+      version = "~> 1.15"
     }
     time = {
-      source = "hashicorp/time"
+      source  = "hashicorp/time"
+      version = ">= 0.9.1"
     }
   }
   cloud {
